02c2cbf6b32f983d6553d3850667cf3d5ece4d9c
JJang95 <jojang1994@gmail.com>
2023-04-07 01:52:10 -0400

    Finished SpringConfig.java

--- SpringThree/src/main/java/org/genspark/SpringConfig.java
+++ SpringThree/src/main/java/org/genspark/SpringConfig.java
@@ -1,5 +1,30 @@
 package org.genspark;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfig {
+
+    @Bean
+    public Student student() {
+        return new Student(1, "Jonathan Jang", phones(), address());
+    }
+
+    @Bean
+    public List<Phone> phones() {
+        List<Phone> phones = new ArrayList<Phone>();
+        phones.add(new Phone("1234567890"));
+        phones.add(new Phone("1234567891"));
+        return phones;
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("Newark", "NJ", "USA", "07101");
+    }
 
 }
